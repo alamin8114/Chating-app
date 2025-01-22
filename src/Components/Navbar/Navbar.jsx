b1d856eb00741c51694c59ae303588e03eb9ea98
user.name :"Alamin Islam <user.email :2003alaminislam28@gmail.com>
2025-01-23 00:04:49 +0600

    navbar set

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,26 +1,44 @@
-import { useSelector } from 'react-redux';
-import chatinglogo from '../../assets/images/chating-nav-removebg-preview.png'
-import { IoIosLogOut } from "react-icons/io";
-import store from '../../store';
+
+import { useState } from 'react'
+import './Navbar.css'
+import { FaBars } from "react-icons/fa"
+import { LuCircleUserRound } from "react-icons/lu";
+import { HiOutlineUsers } from "react-icons/hi";
+import { LuMessageSquareMore } from "react-icons/lu";
+import { FiUserCheck } from "react-icons/fi";
+import { FiUserMinus } from "react-icons/fi";
+import { FiUserPlus } from "react-icons/fi";
 const Navbar = () => {
-    const data =useSelector((store)=>store.data.value)
-    console.log(data)
+    const[show,setshow]=useState(false)
+    // const data =useSelector((store)=>store.data.value)
+    // console.log(data)
   return (
     <>
-    <div className="alamin-nav py-3 bg-[#C890A7]">
+    <div className="alamin-nav  bg-[#E8F9FF]">
         <div className="container">
-            <div className="nav-row flex justify-around">
-                <div className="navlogo">
-                    <img className='w-[50px] rounded-full' src={chatinglogo} alt="logo" />
+        <div className="main-menu">
+            <div className={`main-nav ${show?'openmenu' : 'closemenu'}`}>
+                <div className={`icons icons1 ${show?'showicon':'hiddenicon'}`}>
+                    <ul>
+                        <li><LuCircleUserRound/>
+                        <p>Profile</p>
+                        </li>
+                        <li><HiOutlineUsers/><p>User</p></li>
+                        <li><LuMessageSquareMore/><p>Message</p></li>
+                    </ul>
                 </div>
-                <div className="mein-menu">
-                    <ul className='flex items-center gap-6'>
-                        <li className='text-[20px] font-Poppins font-bold text-white'>{store.displayName}</li>
-                        <li className='w-[50px] h-[50px] border-2 border-[#471f30] bg-gray-300 rounded-full'></li>
-                        <li className='text-[24px] hover:text-black transition-all duration-[.4s] hover:scale-[1.1] text-white'><IoIosLogOut/></li>
+                <button onClick={()=>setshow(!show) } className="barbutton">
+                    <FaBars/>
+                </button>
+                <div  className={`icons icons2 ${show?'showicon':'hiddenicon'}`}>
+                    <ul>
+                        <li><FiUserCheck/><p>Request</p></li>
+                        <li><FiUserPlus/><p>Friends</p></li>
+                        <li><FiUserMinus/><p>Blocked</p></li>
                     </ul>
                 </div>
             </div>
+        </div>
         </div>
     </div>
     </>
